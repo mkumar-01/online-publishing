02998258b62f60bcacf4ab051e5f1c9c5f343b3c
mkumar-01 <manoj.kumar@nagarro.com>
2025-07-09 12:45:21 +0530

    added new articles and authors other articles

--- src/app/pages/articledetail/articledetail.component.ts
+++ src/app/pages/articledetail/articledetail.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Post, PostResponse } from '../../store/models/posts.model';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Post, } from '../../store/models/posts.model';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/reducers';
-import { Authors } from '../../store/models/authors.model';
 import * as PostsActions from '../../store/actions/posts.actions';
 import * as ArticleActions from '../../store/actions/articleDetail.actions';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'articledetail',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './articledetail.component.html',
   styleUrl: './articledetail.component.scss'
 })
@@ -16,29 +16,51 @@ export class ArticledetailComponent implements OnInit {
   private store = inject<Store<AppState>>(Store);
   public id: string | null = null;
   public articalDetail = signal<Post | null>(null);
-  public allData = signal<PostResponse | null>(null);
+  public sameAuthorArticles = signal<Post[]>([]);
+  public posts = signal<Post[] | null>(null);
   private activatedRoute = inject(ActivatedRoute);
   private endPoint = 'http://localhost:3000/posts';
   constructor() {
     this.id = this.activatedRoute.snapshot.paramMap.get('id')
   }
   ngOnInit(): void {
-    if (this.id) {
-      this.store.dispatch(ArticleActions.loadArticleDetail({ endPoint: this.endPoint + "/" + this.id }));
-    }
-
-    this.store.dispatch(PostsActions.loadPosts({ endPoint: this.endPoint }));
-    this.store.select(state => state?.article?.articleData).subscribe(res => {
-      if (res) {
-        this.articalDetail.set(res)
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      if (this.id) {
+        this.store.dispatch(ArticleActions.loadArticleDetail({ endPoint: this.endPoint + "/" + this.id }));
       }
-    });
-    this.store.select(state => state?.posts?.data).subscribe(res => {
-      if (res) {
-        this.allData.set(res);
-      }
-    });
 
+      this.store.dispatch(PostsActions.loadPosts({ endPoint: this.endPoint }));
+      this.store.select(state => state?.article?.articleData).subscribe(res => {
+        if (res) {
+          this.articalDetail.set(res)
+
+        }
+      });
+      this.store.select(state => state?.posts?.data).subscribe(res => {
+        if (res) {
+          // const postsArray = res as unknown as Post[];
+          const postsArray = Array.isArray(res) ? res : [];
+          this.posts.set(postsArray);
+        }
+        this.getAuthorArticles()
+      });
+    })
+
+
+  }
+  getAuthorArticles() {
+    const currentArticle = this.articalDetail();
+    const articles = this.posts();
+    if (!currentArticle || !articles) return;
+    const relatedArticles = articles?.filter(item => {
+      return item.authorName.toLowerCase() === currentArticle.authorName.toLowerCase() &&
+        item.id !== currentArticle.id;
+    })
+    this.sameAuthorArticles.set(relatedArticles);
+    console.log(this.sameAuthorArticles())
+  }
+  getRelatedArticlesByTags() {
 
   }
 
